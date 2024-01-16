0bf4d62137d400bd2dc2eb041bfa823535909f8a
katewitkoski <ka.lwitkoski@gmail.com>
2024-01-16 17:14:25 -0300

    torna o menu responsivo

---
+++ static/script.js
@@ -0,0 +1,13 @@
+// Abre e fecha menulateral em modo mobile
+const menuMobile = document.querySelector(".menu-mobile");
+const body = document.querySelector("body");
+
+// if ternário
+menuMobile.addEventListener("click", () => {
+    alert("entrou no evento")
+    menuMobile.classList.contains("bi-list")
+    ? menuMobile.classList.replace("bi-list", "bi-x")
+    : menuMobile.classList.replace("bi-x","bi-list");
+    body.classList.toggle("menu-nav-active");
+});
+
