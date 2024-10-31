71bdbdfd85e14dd729f36dcc8f2864e2d530e965
katewitkoski <ka.lwitkoski@gmail.com>
2024-10-31 12:12:47 -0300

    tenta resolver o 404, modifica rota

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ def index():
     return render_template('index.html')  # Renderiza o index.html diretamente
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8000)
