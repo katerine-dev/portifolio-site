f04d6a3789e382f4330a45291b5bbaa798a606ea
katewitkoski <ka.lwitkoski@gmail.com>
2024-10-31 15:01:32 -0300

    volta o estado no app.py

--- app.py
+++ app.py
@@ -1,11 +1,62 @@
-from flask import Flask, render_template
+# configurar o servidor flask
 
-app = Flask(__name__, static_folder='static')
-#app.secret_key = 'katecode'
+from flask import Flask, render_template, redirect, request, flash
+# from flask_mail import Mail, Message
+import dotenv
+import os
 
-@app.route('/')  # Rota para a página principal
+dotenv.load_dotenv(dotenv.find_dotenv())
+
+app = Flask(__name__)
+app.secret_key = 'katecode'
+
+# mail_settings = {
+#     "MAIL_SERVER": "smtp.gmail.com",
+#     "MAIL_PORT": 465,
+#     "MAIL_USE_SSL": True,
+#     "MAIL_USE_TLS": False,
+#     "MAIL_USERNAME": os.getenv("USERNAME"),
+#     "MAIL_PASSWORD": os.getenv("PASSWORD")
+# }
+
+# app.config.update(mail_settings)
+# mail = Mail(app)
+
+# class Contact:
+#     def __init__(self, name, email, message):
+#         self.name = name
+#         self.email = email
+#         self.message = message
+
+@app.route('/') # uma página só 
 def index():
-    return render_template('index.html')  # Renderiza o index.html diretamente
+    return render_template('index.html') 
+
+# @app.route('/send/email', methods=['GET', 'POST'])
+# def send():
+#     if request.method == 'POST':
+#         formContact = Contact(
+#             request.form['name'],
+#             request.form['email'],
+#             request.form['message']
+#         )
+
+#         msg = Message(
+#             subject= f'{formContact.name} te enviou uma mensagem pelo site-portifólio',
+#             sender=app.config.get("MAIL_USERNAME"),
+#             recipients=[app.config.get("MAIL_USERNAME")],
+#             body = f'''
+
+#             {formContact.name} com o email {formContact.email}, te enviou a seguinte mensagem:
+
+#             {formContact.message}
+
+
+#             '''
+#         )
+#         mail.send(msg)
+#         flash('Message sent successfully! Thank you for contacting me!')
+#     return redirect('/')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True) # para conseguir dar F5 
