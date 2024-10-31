7ddf25cebf4250e51463885eb76f108ff5a31500
katewitkoski <ka.lwitkoski@gmail.com>
2024-10-31 12:33:38 -0300

    tenta resolver o 404, modifica rota

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
 
 app = Flask(__name__, static_folder='static')
-app.secret_key = 'katecode'
+#app.secret_key = 'katecode'
 
 @app.route('/')  # Rota para a página principal
 def index():
     return render_template('index.html')  # Renderiza o index.html diretamente
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=5000)
